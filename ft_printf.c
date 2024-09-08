29abe5374e9f2eda4a03e43260b7ae7a31c4d7a5
Enrique Gil Piqueras <enrgil-p@c1r5s1.42madrid.com>
2024-09-08 21:37:47 +0200

    Written comments of what I need know

--- ft_printf.c
+++ ft_printf.c
@@ -6,22 +6,33 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 18:13:45 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/09/08 20:57:45 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/09/08 21:36:47 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_printf.h>
 
-int ft_printf(char const *str, ...)/*Check if char const * need a var name*/
+int	ft_printf(char const *str, ...)/*Check if char const * need a var name*/
 {
-		size_t	index;
-		int		result;
+	int		result;
 
-		result = -1;
-		if (str == NULL)
-				return (result);
-		index = 0;
-		while (str[index] != '\0')
+	result = 0;
+	if (str == NULL)
+		return (-1);
+	result = 0;
+	while (str[result] != '\0')
+	{
+		if (str[result] == %)/*va_* macros's time*/
+		{	/*go to next char, see what is the argument*/
+			/*Be sure that exist an argument above*/
+			/*write argument and be sure we count chars written there*/
+			/*result + num_written*/
+		}
+		else
+			putchar/**/;
+		result++;
+	}
+	return (result);
 }
 /*
 Okay, what do we have here?
@@ -36,5 +47,6 @@ Two parameters
 /*
 We must use write to print. 
 We will write char until we find %. We check by what character is followed it.
-We will go then to another function and see how to convert the data in chars, and then write it
+We will go then to another function and see how to convert the data in chars,
+and then write it
 */
