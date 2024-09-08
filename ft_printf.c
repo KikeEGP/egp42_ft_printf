6edfe4e85944c8f099147407424e5a556ef14144
Enrique Gil Piqueras <enrgil-p@c1r5s1.42madrid.com>
2024-09-08 16:08:56 +0200

    Notes at *.c. Macros for hexa_base at *.h

--- ft_printf.c
+++ ft_printf.c
@@ -6,8 +6,25 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 18:13:45 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/09/03 18:13:55 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/09/08 16:04:33 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ft_printf.h>
 
+int ft_printf(char const *, ...)/*Check if char const * need a var name*/
+/*
+Okay, what do we have here?
+
+Return value expected is an int
+
+Two parameters
+	The string "string with %d and similar",
+	unknown number of vars, which are the data expected to be printed inside
+	string
+*/
+/*
+We must use write to print. 
+We will write char until we find %. We check by what character is followed it.
+We will go then to another function and see how to convert the data in chars, and then write it
+*/
--- ft_printf.h
+++ ft_printf.h
@@ -6,10 +6,13 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 15:03:38 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/09/07 15:13:30 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/09/08 16:08:21 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>/*For write()*/
 #include <stdlib.h>/*For malloc() and free()*/
 #include <stdarg.h>/*For va_list, va_start(), va_arg(), va_copy(), va_end()*/
+
+#define UPPER_HEXA_BASE "0123456789ABCDEF"
+#define LOWER_HEXA_BASE "0123456789abcdef"
