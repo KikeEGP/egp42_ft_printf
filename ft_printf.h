8ad32051679ad215023a5b2dd4446c04cfa6df5d
Enrique Gil Piqueras <enrgil-p@c1r5s1.42madrid.com>
2024-09-08 15:34:18 +0200

    Created ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,10 +6,10 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 15:03:38 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/09/07 15:11:49 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/09/07 15:13:30 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>/*For write()*/
 #include <stdlib.h>/*For malloc() and free()*/
-#include <stdarg.h>/*For va_start(), va_arg(), va_copy(), va_end()*/
+#include <stdarg.h>/*For va_list, va_start(), va_arg(), va_copy(), va_end()*/
