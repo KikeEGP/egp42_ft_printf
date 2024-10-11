4d7481e4ea0c4872642930ac4d6bd2f5db450bac
Enrique Gil Piqueras <enrgil-p@c1r10s1.42madrid.com>
2024-10-11 20:04:02 +0200

    Merged decimal and hexadecimal in this file. manage_number.c

---
+++ print_functions/manage_number.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   manage_number.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 16:18:58 by enrgil-p          #+#    #+#             */
+/*   Updated: 2024/10/11 19:58:56 by enrgil-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_printf.h"
+
+int	print_number_base(int num, int base)
+/*Function to write digit, for d, i and u*/
+/*11-10-24	use it for x, X and p too, adding base as a parameter*/
+
+int	print_integer()/*For d and i*/
+{
+	/*Function call to putnbr()*/
+}
+
+int	print_unsigned_decimal()/*For u*/
+{
+	/*Need to cast int to unsgined? Or var does itself?*/
+	/*Here, insert putnbr*/
+}
