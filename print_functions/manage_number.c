2b32aec9a339b760db53c9dff2cbbf41fac393d4
Enrique Gil Piqueras <enrgil-p@c1r10s1.42madrid.com>
2024-10-11 22:23:55 +0200

    putnbr_base is almost done. Check howto call UPPER_HEXA_BASE when you are calling with %X, only uppercase specifier

--- print_functions/manage_number.c
+++ print_functions/manage_number.c
@@ -6,15 +6,33 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 16:18:58 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/10/11 19:58:56 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/10/11 22:21:31 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-int	print_number_base(int num, int base)
 /*Function to write digit, for d, i and u*/
 /*11-10-24	use it for x, X and p too, adding base as a parameter*/
+int	putnbr_base(int num, int base)
+{
+	int	result;
+	unsigned int	unsign;
+
+	result = 0;
+	if (num < 0 && base == 10)
+	{
+		result += print_char('-');
+		unsign = num * -1;
+	}
+	else
+		unsign = num;
+	if (unsign >= base)
+		result += putnbr_base((unsign / base), base);
+	result += print_char(LOWER_HEXA_BASE[unsign % base]);
+	/*HOW CAN I SWITCH TO UPPER ONLY IN CASE OF %X, only uppercase specifier*/
+	return (result);
+}
 
 int	print_integer()/*For d and i*/
 {
