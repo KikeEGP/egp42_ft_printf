cf6f92b1258867fa69cf62db1f553adf69271564
Enrique Gil Piqueras <enrgil-p@c2r11s6.42madrid.com>
2024-10-22 20:10:44 +0200

    Corrected %#X: in case of 0, just print 0

--- print_functions/manage_extra_bonus.c
+++ print_functions/manage_extra_bonus.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 14:46:06 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/10/21 15:16:35 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/10/22 20:09:17 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,15 +38,20 @@ int	print_hash_hexadecimal_bonus(int hexa_argument, int specifier)
 	int		result;
 
 	result = 0;
-	if (specifier == 'x')
+	if (hexa_argument != 0)
 	{
-		result += print_string("0x");
+		if (specifier == 'x')
+		{
+			result += print_string("0x");
+			result += putnbr(hexa_argument, LOWER_HEXA_BASE, 16);
+		}
+		else if (specifier == 'X')
+		{
+			result += print_string("0X");
+			result += putnbr(hexa_argument, UPPER_HEXA_BASE, 16);
+		}
+	}
+	else
 		result += putnbr(hexa_argument, LOWER_HEXA_BASE, 16);
-	}
-	else if (specifier == 'X')
-	{
-		result += print_string("0X");
-		result += putnbr(hexa_argument, UPPER_HEXA_BASE, 16);
-	}
 	return (result);
 }
