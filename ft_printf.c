4594a032e927ae9202f7434bc8319c38408ffa6e
Enrique Gil Piqueras <enrgil-p@c1r9s5.42madrid.com>
2024-10-23 14:06:43 +0200

    Deleted initial ideas to print in case of arguments and without theme

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 18:13:45 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/10/11 22:19:32 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/10/23 14:05:31 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,46 +53,16 @@ static int	write_with_arguments(char const *str, va_list  parameter)
 	}
 }
 
-int	ft_printf(char const *str, ...)/*Check if char const * need a var name*/
+int	ft_printf(char const *format, ...)
 {
 	int		result;
-	int		check;
 	va_list	parameter;
 
 	if (str == NULL)
 		return (-1);
 	result = 0;
-	check = check_if_specifier(str);
-	//va_start(parameter, str);
-	if (check > 1)
-	{
-			/*function with va_list. This will check if it's properly used %*/
-			/*TEST IT COMPARING ORIGINAL, BEFORE YOU DO MORE
-			CHECK IF PRINTF(STR, ARG1) WITH NO % COMPILES,
-			AND SAME WITH YOURS*/
-		result += write_with_arguments(str, parameter);
-		va_end(parameter);
-	}
-	else
-	{
-		result += write_percent_or_zero_arg(str);
-	//	va_end(parameter);
-	}
+	va_start(parameter, format);
+	result += write_with_arguments(format, parameter);
+	va_end(parameter);
 	return (result);
 }
-/*
-Okay, what do we have here?
-
-Return value expected is an int
-
-Two parameters
-	The string "string with %d and similar",
-	unknown number of vars, which are the data expected to be printed inside
-	string
-*/
-/*
-We must use write to print. 
-We will write char until we find %. We check by what character is followed it.
-We will go then to another function and see how to convert the data in chars,
-and then write it
-*/
