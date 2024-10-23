3083320c9dbf049ef163816434228acd31991f09
Enrique Gil Piqueras <enrgil-p@c1r9s5.42madrid.com>
2024-10-23 14:39:56 +0200

    How must be done % specifier?

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 18:13:45 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/10/23 14:29:09 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/10/23 14:39:30 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,9 @@ static int	check_argument_and_write(const char *specifier, va_list ap)
 		return (putnbr_base(va_arg(ap, unsigned int), LOWER_HEXA_BASE, 10));
 	else if (specifier == 'x' || specifier == 'X')
 		return (print_hexadecimal(va_arg(ap, unsigned int), specifier));
+	/*else
+		while (specifier != %)*/
+		
 }
 
 static int	print_format(char const *format, va_list  parameter)
