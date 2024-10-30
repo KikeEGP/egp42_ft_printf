5b80a68281c9bfeb9de7060f5519a14eba919c3e
enrique_gil_piqueras <131750246+KikeEGP@users.noreply.github.com>
2024-10-30 10:15:13 +0100

    Created check_flags

--- bonus/flag_characters_bonus.c
+++ bonus/flag_characters_bonus.c
@@ -1,1 +1,14 @@
 
+int  check_flags(char flag, char specifier, va_list ap)
+{
+  int  result;
+
+  result = 0;
+  if ((flag == '+') && ((specifier == 'd') || (specifier == 'i')))
+    result += print_flag_plus(va_arg(ap, int))
+  else if ((flag == 32) && ((specifier == 'd') || (specifier == 'i')))
+    result += print_flag_space(va_arg(ap, int));
+  else if ((flag == '#') && ((specifier == 'x') || (specifier == 'X')))
+    result += print_flag_hash(va_arg(ap, unsigned int), specifier);
+  return (result);
+}
